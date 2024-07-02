d6431054a5aea42318ec82873984bd13513bc444
ht <hi@how.hadi.is>
2024-07-01 21:58:34 -0700

    Create grid

---
+++ src/components/Cell.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+withDefaults(defineProps<{name: string}>(), {name: 'grid'});
+</script>
+
+<template>
+    <input type="radio" :name="name"></input>
+</template>
+
+<style scoped>
+    input {
+        display: block;
+    }
+</style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { getCurrentInstance } from 'vue';
+import Cell from './Cell.vue'
+
+defineProps<{width: number, height: number}>();
+
+const uid = getCurrentInstance()?.uid.toString();
+</script>
+
+<template>
+    <div>
+        <template v-for="_ in height">
+            <Cell v-for="_ in width" :name="uid"/>
+        </template>
+    </div>
+</template>
+
+<style scoped>
+div {
+    display: grid;
+    grid-template: repeat(v-bind(height), 1fr) / repeat(v-bind(width), 1fr);
+}
+</style>
