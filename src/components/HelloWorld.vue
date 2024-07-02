379124cd6d15661ec6f7a8354d1876b85d76a159
ht <hi@how.hadi.is>
2024-07-01 22:06:23 -0700

    Update grid

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
 import Grid from './Grid.vue';
 
+let width = ref(10);
+let height = ref(10);
 </script>
 
 <template>
-  <Grid :width="5" :height="6"></Grid>
+  Width
+  <input placeholder="Width" type="number" v-model.number="width">
+  Height
+  <input placeholder="Height" type="number" v-model.number="height">
 
+
+  <Grid :width :height></Grid>
 </template>
 
 <style scoped>
